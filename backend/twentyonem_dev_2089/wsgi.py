79b22d814d39510b02c9cafa1339bb4eb621fdd9
Crowdbotics <team@crowdbotics.com>
2020-03-21 09:53:19 +0000

    Update models

--- backend/twentyonem_dev_2089/wsgi.py
+++ backend/twentyonem_dev_2089/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twentyonem_dev_2089.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twentyonem_dev_2089.settings")
 
 application = get_wsgi_application()
